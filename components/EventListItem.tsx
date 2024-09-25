11b425fa91e7dbbe8188ae995e89ed6c5862c4cd
BarocoCode <dragossnicolae19@gmail.com>
2024-09-25 19:11:11 +0300

    Event details page

--- components/EventListItem.tsx
+++ components/EventListItem.tsx
@@ -1,30 +1,35 @@
 import Feather from '@expo/vector-icons/Feather';
 import dayjs from 'dayjs';
-import { View, Text, Image } from 'react-native';
+import { Link } from 'expo-router';
+import { View, Text, Image, Pressable } from 'react-native';
 
 export default function EventListItem({ event }) {
   return (
-    <View className="m-3 gap-3 border-b border-gray-200 pb-3">
-      <View className="flex-row">
-        <View className="flex-1 gap-2">
-          <Text className="text-lg font-semibold uppercase text-amber-700">
-            {dayjs(event.datetime).format('ddd, D MMM')} · {dayjs(event.datetime).format('h:mm A')}
-          </Text>
-          <Text className="text-2xl font-bold" numberOfLines={2}>
-            {event.title}
-          </Text>
-          <Text className="text-gray-700">{event.location}</Text>
-        </View>
-        {/* Event Image */}
-        <Image source={{ uri: event.image }} className="aspect-video w-2/5 rounded-xl" />
-      </View>
+    <Link href={`/${event.id}`} asChild>
+      <Pressable className="m-3 gap-3 border-b border-gray-200 pb-3">
+        <View className="flex-row">
+          <View className="flex-1 gap-2">
+            <Text className="text-lg font-semibold uppercase text-amber-700">
+              {dayjs(event.datetime).format('ddd, D MMM')} ·{' '}
+              {dayjs(event.datetime).format('h:mm A')}
+            </Text>
+            <Text className="text-2xl font-bold" numberOfLines={2}>
+              {event.title}
+            </Text>
 
-      {/* Footer*/}
-      <View className="flex-row gap-3">
-        <Text className="mr-auto text-gray-700">16 Going</Text>
-        <Feather name="share" size={20} color="gray" />
-        <Feather name="bookmark" size={24} color="gray" />
-      </View>
-    </View>
+            <Text className="text-gray-700">{event.location}</Text>
+          </View>
+          {/* Event Image */}
+          <Image source={{ uri: event.image }} className="aspect-video w-2/5 rounded-xl" />
+        </View>
+
+        {/* Footer*/}
+        <View className="flex-row gap-3">
+          <Text className="mr-auto text-gray-700">16 Going</Text>
+          <Feather name="share" size={20} color="gray" />
+          <Feather name="bookmark" size={24} color="gray" />
+        </View>
+      </Pressable>
+    </Link>
   );
 }
