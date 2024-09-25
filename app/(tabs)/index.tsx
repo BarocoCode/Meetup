0019be3033a35fcf1f2a1243ab9879e89ecc5e23
Smart Sante <smartsante@Smarts-MacBook-Pro.local>
2024-09-25 17:58:26 +0300

    Event list item, styles with nativewind, custom component, props

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,17 @@
 import { Stack } from 'expo-router';
-import { View } from 'react-native';
+
+import events from '~/assets/events.json';
+import EventListItem from '~/components/EventListItem';
 
 export default function Events() {
   return (
     <>
       <Stack.Screen options={{ title: 'Events' }} />
+
+      {/* Event list item*/}
+      <EventListItem event={events[0]} />
+      <EventListItem event={events[1]} />
+      <EventListItem event={events[2]} />
     </>
   );
 }
